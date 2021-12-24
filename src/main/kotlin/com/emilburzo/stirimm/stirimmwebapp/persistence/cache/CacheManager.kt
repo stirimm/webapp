d40121d39861c7eeab04fa7e0a800127a033f3b7
Emil Burzo <contact@emilburzo.com>
2021-12-24 16:49:56 +0200

    add caching

---
+++ src/main/kotlin/com/emilburzo/stirimm/stirimmwebapp/persistence/cache/CacheManager.kt
@@ -0,0 +1,26 @@
+package com.emilburzo.stirimm.stirimmwebapp.persistence.cache
+
+import com.github.benmanes.caffeine.cache.Caffeine
+import org.springframework.cache.CacheManager
+import org.springframework.cache.caffeine.CaffeineCache
+import org.springframework.cache.support.SimpleCacheManager
+import org.springframework.context.annotation.Bean
+import java.util.concurrent.TimeUnit
+
+
+@Bean
+fun cacheManager(): CacheManager {
+    val cacheManager = SimpleCacheManager()
+    val newsCache = CaffeineCache(
+        CACHE_NAME_RECENT_NEWS,
+        Caffeine.newBuilder()
+            .expireAfterWrite(3, TimeUnit.MINUTES)
+            .maximumSize(1)
+            .recordStats()
+            .build()
+    )
+    cacheManager.setCaches(listOf(newsCache))
+    return cacheManager
+}
+
+const val CACHE_NAME_RECENT_NEWS = "cache_recent_news"
--- src/main/kotlin/com/emilburzo/stirimm/stirimmwebapp/service/NewsService.kt
+++ src/main/kotlin/com/emilburzo/stirimm/stirimmwebapp/service/NewsService.kt
@@ -2,6 +2,8 @@ package com.emilburzo.stirimm.stirimmwebapp.service
 
 import com.emilburzo.stirimm.stirimmwebapp.persistence.News
 import com.emilburzo.stirimm.stirimmwebapp.persistence.NewsRepository
+import com.emilburzo.stirimm.stirimmwebapp.persistence.cache.CACHE_NAME_RECENT_NEWS
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 
 @Service
@@ -9,6 +11,7 @@ class NewsService constructor(
     private val repository: NewsRepository
 ) {
 
+    @Cacheable(CACHE_NAME_RECENT_NEWS)
     fun findRecent(): Iterable<News> {
         return repository.findTop200ByOrderByPublishDateDesc()
     }
