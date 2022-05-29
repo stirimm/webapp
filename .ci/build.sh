039c0b88ca15c881a2465c3572ffb079d46507a0
Emil Burzo <contact@emilburzo.com>
2022-05-29 11:25:10 +0300

    add mvn build step

--- .ci/build.sh
+++ .ci/build.sh
@@ -5,4 +5,6 @@ set -eux
 NAME="registry.emilburzo.com/stirimm-webapp"
 PLATFORMS="linux/amd64,linux/arm64"
 
+mvn clean package
+
 docker buildx build --push --platform ${PLATFORMS} -t ${NAME} -t ${NAME}:${BUILD_NUMBER} .
